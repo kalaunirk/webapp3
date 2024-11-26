163ea3eaa7cd8cc3f4b09d2c2c76bf3b0ead1222
Rohit Kalauni <rkkalaunirohit@gmail.com>
2024-11-26 08:20:13 +0545

    reposftfirst commit

---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication3.Models;
+
+public class Student
+{
+    public int StudentID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public DateTime EnrollmentData { get; set; }
+    public string Address{ get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string CreatedBy { get; set; }
+    
+
+} 
